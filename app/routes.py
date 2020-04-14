8602b625d4408ce20df14430d763bc4aa037aeae
Rotherford Marmibe <rmarmibe@gmail.com>
2020-04-14 13:51:52 +0800

    update blog

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,23 @@ from app import app
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('blog.html')
+    head_post = {'title': 'Top post body' , 'body': ' Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.' , 'date': 'Feb 12'}
+    post1 = {'title': 'Featured post', 'body': 'This is a wider card with supporting text below as a natural lead-in to additional content.', 'date': 'Feb 11'}
+    post2= {'title': 'Post 2', 'body': 'This is a wider card with supporting text below as a natural lead-in to additional content.', 'date': 'Feb 10'}
+    topics = [
+        'World',
+        'U.S.',
+        'Technology',
+        'Design',
+        'Culture',
+        'Business',
+        'Politics',
+        'Opinion',
+        'Science',
+        'Health',
+        'Style',
+        'Travel',
+    ]
+
+    return render_template('blog.html',topics=topics, head_post=head_post, post1=post1, post2=post2)
 
